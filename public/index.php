686e10af4cc48f01f26e882f53b7bc36bff384a5
Eloise-git <eloise.proqueff@gmail.com>
2023-10-23 14:59:45 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Add Slim routing middleware
+$app->addRoutingMiddleware();
+
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$app->add(new BasePathMiddleware($app));
+
+$app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', function (Request $request, Response $response) {
+  $response->getBody()->write('Hello, World!');
+  return $response
+    ->withHeader('Content-Type', 'application/json')
+    ->withStatus(200);
+})->setName('root');
+
+// Run app
+$app->run();
