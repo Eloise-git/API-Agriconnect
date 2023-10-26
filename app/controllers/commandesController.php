61ae6547ceadf422fb57bf2689554b4c5ed09647
Eloise-git <eloise.proqueff@gmail.com>
2023-10-26 09:46:05 +0200

    Add try+catch in all controller, and create producerController

--- app/controllers/commandesController.php
+++ app/controllers/commandesController.php
@@ -20,54 +20,74 @@ class CommandesController
   //Permet d'obtenir la liste des commandes
   public function getAllCommandes(RequestInterface $request, ResponseInterface $response, array $args)
   {
-    $order = $this->db->query('SELECT * FROM commande');
-    $response->getBody()->write(json_encode($order));
-    return $response;
+    try{
+      $order = $this->db->query('SELECT * FROM commande');
+      $response->getBody()->write(json_encode($order));
+      return $response;
+    }catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 
   //Permet d'obtenir les informations d'une commande
   public function getACommande(RequestInterface $request, ResponseInterface $response, array $args)
   {
-    $id_orderWanted = $args['id'];
-    $order = $this->db->query("SELECT * FROM commande WHERE id_commande ='$id_commandeWanted");
-    $response->getBody()->write(json_encode($order));
-    return $response;
+    try{
+      $id_orderWanted = $args['id'];
+      $order = $this->db->query("SELECT * FROM commande WHERE id_commande ='$id_commandeWanted");
+      $response->getBody()->write(json_encode($order));
+      return $response;
+    }catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 
   //Permet d'ajouter une commande
   public function postCommande(RequestInterface $request, ResponseInterface $response, array $args)
   {
-    $id_orderWanted = $args['id'];
-    $status_orderWanted = $args['status'];
-    $date_orderWanted = $args['date'];
-    $payement_orderWanted = $args['payement'];
-    $id_producer_orderWanted = $args['id_producer'];
-    $id_user_orderWanted = $args['id_user'];
-    $order = $this->db->query("INSERT INTO commande VALUES ($id_orderWanted','$status_orderWanted','$date_orderWanted','$payement_orderWanted','$id_producer_orderWanted','$id_user_orderWanted';");
-    $response->getBody()->write(json_encode($order));
-    return $response;
+    try{
+      $id_orderWanted = $args['id'];
+      $status_orderWanted = $args['status'];
+      $date_orderWanted = $args['date'];
+      $payement_orderWanted = $args['payement'];
+      $id_producer_orderWanted = $args['id_producer'];
+      $id_user_orderWanted = $args['id_user'];
+      $order = $this->db->query("INSERT INTO commande VALUES ($id_orderWanted','$status_orderWanted','$date_orderWanted','$payement_orderWanted','$id_producer_orderWanted','$id_user_orderWanted';");
+      $response->getBody()->write(json_encode($order));
+      return $response;
+    } catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 
   //Permet de mettre à jour les informations d'une commande
   public function putCommande(RequestInterface $request, ResponseInterface $response, array $args)
   {
-    $id_orderWanted = $args['id'];
-    $status_orderWanted = $args['status'];
-    $date_orderWanted = $args['date'];
-    $payement_orderWanted = $args['payement'];
-    $id_producer_orderWanted = $args['id_producer'];
-    $id_user_orderWanted = $args['id_user'];
-    $order = $this->db->query("UPDATE commande SET id_order='$id_orderWanted', status_order='$status_orderWanted', date_order='$date_orderWanted', payement_order='$payement_orderWanted', id_producer='$id_producer_orderWanted', id_user='$id_user_orderWanted';");
-    $response->getBody()->write(json_encode($order));
-    return $response;
+    try{
+      $id_orderWanted = $args['id'];
+      $status_orderWanted = $args['status'];
+      $date_orderWanted = $args['date'];
+      $payement_orderWanted = $args['payement'];
+      $id_producer_orderWanted = $args['id_producer'];
+      $id_user_orderWanted = $args['id_user'];
+      $order = $this->db->query("UPDATE commande SET id_order='$id_orderWanted', status_order='$status_orderWanted', date_order='$date_orderWanted', payement_order='$payement_orderWanted', id_producer='$id_producer_orderWanted', id_user='$id_user_orderWanted';");
+      $response->getBody()->write(json_encode($order));
+      return $response;
+    }catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 
   //Permet de supprimer une commande
   public function deleteCommande(RequestInterface $request, ResponseInterface $response, array $args)
   {
-    $id_orderWanted = $args['id'];
-    $order = $this->db->query("DELETE FROM commande WHERE id_order='$id_orderWanted';");
-    $response->getBody()->write(json_encode($order));
-    return $response;
+    try{
+      $id_orderWanted = $args['id'];
+      $order = $this->db->query("DELETE FROM commande WHERE id_order='$id_orderWanted';");
+      $response->getBody()->write(json_encode($order));
+      return $response;
+    }catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 }
--- app/controllers/messagerieController.php
+++ app/controllers/messagerieController.php
@@ -20,48 +20,64 @@ class MessagerieController
   //Permet d'obtenir la liste des messages
   public function getAllMessages(RequestInterface $request, ResponseInterface $response, array $args)
   {
-    $role = '';
+    try{
+        $role = '';
 
-    //Obtention des tokens et vérifications à revoir
-  if (substr($header, 0, 7) !== 'Bearer ') {
-    return false;
-  } else if (substr($header, 0, 7) == $token_producer){
-    $role = 'producer';
-  } elseif (substr($header, 0, 7) == $token_client) {
-    $role = 'client';
-  }
+        //Obtention des tokens et vérifications à revoir
+      if (substr($header, 0, 7) !== 'Bearer ') {
+        return false;
+      } else if (substr($header, 0, 7) == $token_producer){
+        $role = 'producer';
+      } elseif (substr($header, 0, 7) == $token_client) {
+        $role = 'client';
+      }
 
-    $message = $this->db->query('SELECT * FROM messagerie');
-    $response->getBody()->write(json_encode($message));
-    return $response;
+        $message = $this->db->query('SELECT * FROM messagerie');
+        $response->getBody()->write(json_encode($message));
+        return $response;
+    }catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 
   //Permet d'obtenir les informations d'un message
   public function getAMessage(RequestInterface $request, ResponseInterface $response, array $args)
   {
-    $id_messageWanted = $args['id'];
-    $message = $this->db->query('SELECT * FROM messagerie WHERE id_message = $id_messageWanted');
-    $response->getBody()->write(json_encode($message));
-    return $response;
+    try {
+      $id_messageWanted = $args['id'];
+      $message = $this->db->query('SELECT * FROM messagerie WHERE id_message = $id_messageWanted');
+      $response->getBody()->write(json_encode($message));
+      return $response;
+    }catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 
   //Permet d'ajouter un message
   public function postMessage(RequestInterface $request, ResponseInterface $response, array $args){
-    $id_messageWanted = $args['id'];
-    $date_messageWanted = $args['date'];
-    $content_messageWanted = $args['content'];
-    $id_userWanted = $args['id_user'];
-    $id_user1Wanted = $args['id_user1'];
-    $message = $this->db->query("INSERT INTO messagerie VALUES ($id_messageWanted','$date_messageWanted','$content_messageWanted','$id_userWanted','$id_user1Wanted';");
-    $response->getBody()->write(json_encode($message));
-    return $response;
+    try{
+      $id_messageWanted = $args['id'];
+      $date_messageWanted = $args['date'];
+      $content_messageWanted = $args['content'];
+      $id_userWanted = $args['id_user'];
+      $id_user1Wanted = $args['id_user1'];
+      $message = $this->db->query("INSERT INTO messagerie VALUES ($id_messageWanted','$date_messageWanted','$content_messageWanted','$id_userWanted','$id_user1Wanted';");
+      $response->getBody()->write(json_encode($message));
+      return $response;
+    }catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 
   //Permet de supprimer un message
   public function deleteMessage(RequestInterface $request, ResponseInterface $response, array $args){
-    $id_messageWanted = $args['id'];
-    $message = $this->db->query("DELETE FROM messagerie WHERE id_message='$id_messageWanted';");
-    $response->getBody()->write(json_encode($message));
-    return $response;
+    try {
+      $id_messageWanted = $args['id'];
+      $message = $this->db->query("DELETE FROM messagerie WHERE id_message='$id_messageWanted';");
+      $response->getBody()->write(json_encode($message));
+      return $response;
+    }catch (Exception $e) {
+      return $response->withStatus(500)->getBody()->write(json_encode($e->getMessage()));
+    }
   }
 }
