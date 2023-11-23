db5fe503662c2cfba5d9dee691a03dd03b7e75f7
Eloise-git <eloise.proqueff@gmail.com>
2023-11-23 14:33:17 +0100

    changements of commande Controller + service

--- app/services/commandeService.php
+++ app/services/commandeService.php
@@ -59,25 +59,27 @@ class CommandeService extends Service
     public function updateOrderById($id_orderWanted, $status_orderWanted, 
     $date_orderWanted, $payement_orderWanted, $id_producer_orderWanted, $id_user_orderWanted)
     {
-        $sql = "UPDATE commmande SET (status_order=':status', 
-            date_order=':date', payement_order=':payement', 
-            id_producer=':id_producer', id_user=':id_user';) WHERE id_order = :id_order";
+        $sql = "UPDATE commande SET status_order=:status, date_order=:date, payement_order=:payement,
+        id_producer=:id_producer, id_user=:id_user WHERE id_order =:id_order;";
 
         $stmt = $this->db->prepare($sql);
         $stmt->execute([
-            'status_order' => $status_orderWanted,
-            'date_order' => $date_orderWanted,
-            'payement_order' => $payement_orderWanted,
+            'status' => $status_orderWanted,
+            'date' => $date_orderWanted,
+            'payement' => $payement_orderWanted,
             'id_producer' => $id_producer_orderWanted,
             'id_user' => $id_user_orderWanted,
             'id_order' => $id_orderWanted
         ]);
-
-        $order = $this->getOrderById($id_orderWanted);
-        if (!$order) {
-        throw new Exception("Erreur lors de la mise à jour de la commande : " . implode(", ", $stmt->errorInfo()));
-        }
-        return $order;
+        
+        return [
+            'status' => $status_orderWanted,
+            'date' => $date_orderWanted,
+            'payement' => $payement_orderWanted,
+            'id_producer' => $id_producer_orderWanted,
+            'id_user' => $id_user_orderWanted,
+            'id_order' => $id_orderWanted
+        ];
     }
 
     public function deleteOrderById($id)
