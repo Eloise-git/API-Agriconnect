297b7ddc251303e097ae53f6a26d9f1302e5100c
lemoine alex <alexlemoine35@gmail.com>
2023-11-23 15:01:00 +0100

    Refactor getAllStock method to include additional stock information

--- app/services/stockService.php
+++ app/services/stockService.php
@@ -15,12 +15,18 @@ class StockService extends Service
 
     public function getAllStock($id_producer)
     {
-        $sql = "SELECT *,COUNT(id_order)AS 'Reservés' FROM `produit` JOIN contenir ON contenir.id_product=produit.id_product WHERE id_producer = :id_producer GROUP BY contenir.id_product";
+        $sql = "SELECT contenir.id_product,name_product,type_product,stock_product,COUNT(id_order)AS 'Reservés' FROM `produit` JOIN contenir ON contenir.id_product=produit.id_product WHERE id_producer = :id_producer GROUP BY contenir.id_product";
         $stmt = $this->db->prepare($sql);
         $stmt->execute(['id_producer' => $id_producer]);
         $stocks = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        return $stocks;
+        return [
+            "id" => $stocks[1]['id_product'],
+            "name" => $stocks[1]['name_product'],
+            "catégorie" => $stocks[1]['type_product'],
+            "quantité" => $stocks[1]['stock_product'],
+            "disponible"=> $stocks[1]['stock_product']-$stocks[1]['Reservés'],
+            "réservés"=> $stocks[1]['Reservés']
+            ];;
     }
 
     public function getAStockById($id_product, $id_producer)
