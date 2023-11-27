83e1bdfd6f172432940b28d7b747d6672cc5b75d
lemoine alex <alexlemoine35@gmail.com>
2023-11-27 15:25:27 +0100

    Fix API call in getDistance function

--- app/Lib/Distance.php
+++ app/Lib/Distance.php
@@ -1,8 +1,6 @@
 <?php
 namespace App\Lib;
 
-$url= 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=San%20Francisco%7CVictoria%20BC&language=fr-FR&mode=car&origins=Vancouver%20BC%7CSeattle&key=YOUR_API_KEY';
-
 function getDistance($adresse1, $adresse2)
 {
     $adresse1 = str_replace(" ", "+", $adresse1);
@@ -10,7 +8,7 @@ function getDistance($adresse1, $adresse2)
     $apiKey = $_ENV['GOOGLE_API_TOKEN'];
     $url = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations='.$adresse1.'&language=fr-FR&mode=car&origins='.$adresse2.'&key='.$apiKey;
     
-    $data = json_decode($url, true);
+    $data = json_decode(file_get_contents($url), true);
 
     var_dump($url);
     $distance = $data['rows'][0]['elements'][0]['distance']['value'];
