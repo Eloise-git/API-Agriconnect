ce7b4b433af19c9a10bf53e3725e570bf1fc0169
lemoine alex <alexlemoine35@gmail.com>
2023-11-30 16:46:49 +0100

    Fix image verification function

--- app/Lib/ImageVerif.php
+++ app/Lib/ImageVerif.php
@@ -1,16 +1,17 @@
 <?php
 namespace App\Lib;
-use function App\Lib\sendError;
+use Exception;
 
 require_once dirname(__DIR__) . '/Lib/Utils.php';
 
 function verificationImage($imageverif){
     $imageType = $imageverif->getClientMediaType();
-    $fileExtension = explode('/',$imageType)[1];
-    $Extensionautoriser = ['jpg','jpeg','png'];
-    if(!in_array($fileExtension,[''])){
+    if(empty($imageType)){
         throw new Exception("Il n'y a pas d'image", 400);
     }
+    $fileExtension = explode('/',$imageType)[1];
+    $Extensionautoriser = ['jpg','jpeg','png'];
+    
 
     if(!in_array($fileExtension,$Extensionautoriser)){
         throw new Exception("Extension non autoriser", 400);
